175cdb42be63c49795c1583579505147e24af9b7
unknown <nachopena133@gmail.com>
2018-05-29 21:41:09 -0300

    Pato

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,20 @@
+var name
+
+name = prompt('Decime nombre')
+
+console.log('Hola ' + name)
+
+
+
+var num1 = '3'
+
+var num2
+
+num2 = prompt('Decime Numero')
+
+var parseNum1 = parseFloat(num1, 10)
+
+var parseNum2 = parseFloat(num2, 10)
+
+console.log('La suma de ' + num2 + '+' + parseNum1 + ':' + (parseNum2 + parseNum1))
+
