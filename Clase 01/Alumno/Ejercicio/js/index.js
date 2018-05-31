9db83c1dfd92b19683e5f53b864ea15cb66f31f9
unknown <nachopena133@gmail.com>
2018-05-31 20:22:17 -0300

    agregue un ejercicio

--- Clase 01/Alumno/Ejercicio/js/index.js
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -10,11 +10,37 @@ var num1 = '3'
 
 var num2
 
-num2 = prompt('Decime Numero')
+num2 = prompt('Ingresar Numero para sumarle 3')
 
 var parseNum1 = parseFloat(num1, 10)
 
 var parseNum2 = parseFloat(num2, 10)
 
 console.log('La suma de ' + num2 + '+' + parseNum1 + ':' + (parseNum2 + parseNum1))
+
+
+
+var Numero1
+
+Numero1 = prompt('Ingresar numero para restarle 2')
+
+var Numero2 = 2
+
+var parseNumero1 = parseFloat(Numero1, 10)
+
+
+
+
+console.log('la resta de ' + Numero1 + '-' + Numero2 + ': ' + (parseNumero1 - Numero2))
+
+var nume1 = prompt('Escribi primer numero')
+var nume2 = prompt('Escribi segundo numero')
+
+var Parsenume1 = parseFloat(nume1, 10)
+var Parsenume2 = parseFloat(nume2, 10)
+
+console.log('La suma entre los dos numeros que ingresaste es: ' + (Parsenume1 + Parsenume2))
+
+
+
 
---
+++ Clase 02/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,27 @@
+var dia = prompt('ingrese un dia')
+
+switch (dia) {
+  case 'lunes':
+  case 'martes':
+  case 'miercoles':
+  case 'jueves':
+  case 'viernes':
+    console.log('Es un dia habil')
+    break
+
+  case 'viernes':
+  case 'sabado':
+    console.log('Es fin de semana')
+    break
+
+  default:
+    console.log('No es un dia valido')
+}
+
+// if (dia === 'lunes' || dia === 'martes' || dia === 'miercoles' || dia === 'jueves' || dia === 'viernes') {
+//   console.log('Es un dia habil')
+// } else if (dia === 'sabado' || dia === 'domingo') {
+//   console.log('Es fin de semana')
+// } else {
+//   console.log('no es un dia valido')
+// }
